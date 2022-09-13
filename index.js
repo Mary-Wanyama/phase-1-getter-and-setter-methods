f28881ade4119293b5a044f0a47847cf65fb2cbe
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-13 08:33:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your Circle class here
+
+class circle{
+    constructor(radius){
+        this.radius = radius
+    }
+    get diameter (){
+        return 2* this.radius
+    }
+    get circumference(){
+        return 2* this.radius * Math.PI 
+    }
+    get area(){
+        return radius * radius * Math.PI
+    }
+    set diameter (radius){
+        return 2* radius
+    }
+    set circumference(radius){
+        return 2* radius * Math.PI 
+    }
+    set area(radius){
+        return radius * radius * Math.PI
+    }
+}
+
+math.sqrt(this.area/math.pi) = 154
+// circle.circumference = value
+// circle.radius = value
+// circle.diameter = value
